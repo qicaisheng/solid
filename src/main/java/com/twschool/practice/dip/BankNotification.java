6d6cb0fa7d6ca14b66bc05572e72038d42358da6
qicaisheng <qicaisheng@qq.com>
2020-07-21 15:26:56 +0800

    extract BankNotification interface

---
+++ src/main/java/com/twschool/practice/dip/BankNotification.java
@@ -0,0 +1,5 @@
+package com.twschool.practice.dip;
+
+public interface BankNotification {
+    void notify(String account1);
+}
--- src/main/java/com/twschool/practice/dip/SMSNotification.java
+++ src/main/java/com/twschool/practice/dip/SMSNotification.java
@@ -1,6 +1,8 @@
 package com.twschool.practice.dip;
 
-public class SMSNotification {
+public class SMSNotification implements BankNotification {
+    
+    @Override
     public void notify(String account1) {
     }
 }
