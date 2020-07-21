94cc53b0f3959d427d0684d27fc1d9f182c00a6c
qicaisheng <qicaisheng@qq.com>
2020-07-21 18:07:18 +0800

    should_be_turn_on_smart_device_given_smart_home_controller_and_smart_sound_box_device

---
+++ src/main/java/com/twschool/practice/sip/SmartSoundBox.java
@@ -0,0 +1,8 @@
+package com.twschool.practice.sip;
+
+public class SmartSoundBox implements SmartDevice {
+    @Override
+    public void turnOn() {
+        
+    }
+}
--- src/test/java/com/twschool/practice/sip/SmartHomeControllerTest.java
+++ src/test/java/com/twschool/practice/sip/SmartHomeControllerTest.java
@@ -24,4 +24,14 @@ public class SmartHomeControllerTest {
 
         Mockito.verify(smartLamp, Mockito.times(1)).turnOn();
     }
+
+    @Test
+    public void should_be_turn_on_smart_device_given_smart_home_controller_and_smart_sound_box_device() {
+        SmartHomeController smartHomeController = new SmartHomeController();
+
+        SmartSoundBox smartSoundBox = Mockito.mock(SmartSoundBox.class);
+        smartHomeController.turnOn(smartSoundBox);
+
+        Mockito.verify(smartSoundBox, Mockito.times(1)).turnOn();
+    }
 }
